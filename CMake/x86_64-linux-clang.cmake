72c622942932ef0b073d31f8e50b7cff774d936a
Jack <jackbennett0708@gmail.com>
2022-05-18 17:41:36 +0100

    Add clang compilation option

---
+++ CMake/x86_64-linux-clang.cmake
@@ -0,0 +1,9 @@
+# ====================================================== #
+#                  CMake toolchain file                  #
+# -------------------------------------------------------#
+# Reference to compile Orion with Clang instead of the   #
+# default (on Linux) GCC.                                #
+# ====================================================== #
+
+set(CMAKE_C_COMPILER "clang")
+set(CMAKE_CXX_COMPILER "clang++")
