5105367ce47c4dd588ccb72f05ae621cec5efbe3
jabenuk <jackbennett0708@gmail.com>
2022-06-05 17:24:00 +0000

    Deploying to gh-pages from  @ e7292216f4c0a4c445b2221657da0ddf0c6ab5c6 🚀

--- d3/d73/group__group__VkAbstractions__Core.js
+++ d3/d73/group__group__VkAbstractions__Core.js
@@ -2,5 +2,5 @@ var group__group__VkAbstractions__Core =
 [
     [ "Vulkan debugging", "d9/d7f/group__group__VkAbstractions__Core__Debugging.html", "d9/d7f/group__group__VkAbstractions__Core__Debugging" ],
     [ "Vulkan device management", "de/d40/group__group__VkAbstractions__Core__Devices.html", "de/d40/group__group__VkAbstractions__Core__Devices" ],
-    [ "oriCreateInstance", "d3/d73/group__group__VkAbstractions__Core.html#ga6065235c47b1737321ae6a2a89587271", null ]
+    [ "oriCreateInstance", "d3/d73/group__group__VkAbstractions__Core.html#ga758a71e4110b1cf5763bacca510cea0a", null ]
 ];
--- de/d40/group__group__VkAbstractions__Core__Devices.js
+++ de/d40/group__group__VkAbstractions__Core__Devices.js
@@ -1,6 +1,7 @@
 var group__group__VkAbstractions__Core__Devices =
 [
     [ "oriPhysicalDeviceSuitabilityCheckFunc", "de/d40/group__group__VkAbstractions__Core__Devices.html#ga9afb67dafb1e31ce790215dd158cd302", null ],
-    [ "oriCreateLogicalDevice", "de/d40/group__group__VkAbstractions__Core__Devices.html#gadc819a6132c93ed64055742707e8e482", null ],
-    [ "oriEnumerateSuitablePhysicalDevices", "de/d40/group__group__VkAbstractions__Core__Devices.html#ga10901eb9ef4006bc0ce642dd6aeaf78f", null ]
+    [ "oriCreateLogicalDevice", "de/d40/group__group__VkAbstractions__Core__Devices.html#ga29a1f6abd68c65b958517fc37515fe7b", null ],
+    [ "oriEnumerateAvailableQueueFamilies", "de/d40/group__group__VkAbstractions__Core__Devices.html#ga2503e739ea2fd15a5d2be9463a250bfe", null ],
+    [ "oriEnumerateSuitablePhysicalDevices", "de/d40/group__group__VkAbstractions__Core__Devices.html#ga7b1b058cdf6e55a26d5c718066e732a7", null ]
 ];
